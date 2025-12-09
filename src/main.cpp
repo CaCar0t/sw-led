2941d22f9d15b696e21113b110a77da795896f6e
CaCar0t <66122660105@g.lpru.ac.th>
2025-12-09 10:51:11 +0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,12 @@
 #include <Arduino.h>
 
-// กำหนดพินสำหรับ VR
-const int analogPin = A0;
-
-void setup() {
-
-// เริ่มต้น Serial Monitor เพื่อแสดงผล
-  Serial.begin(115200);
-  delay (1000); // รอการเชื่อมต่อ
-  Serial.println("Starting VR Analog Read... ");
+void setup(){
+  pinMode (D1,OUTPUT);
 }
+
 void loop() {
-  // อ่านค่าอนาล็อกจาก VR
-  int vrValue = analogRead(analogPin);
-  // แปลงค่าที่อ่านได้เป็นเปอร์เซ็นต์
-  int percentage = map(vrValue, 0, 1023, 0, 100);
-
-  // แสดงค่าที่อ่านได้และค่าเปอร์เซ็นต์
-  Serial.print("VR Value: ");
-  Serial.print(vrValue);
-  Serial.print(" (");
-  Serial.print(percentage);
-  Serial.println("%)");
-
-  delay(500); // อ่านค่าทุก ๆ 500 ms
+  digitalWrite (D1,HIGH);
+  delay(1000);
+  digitalWrite (D1,LOW);
+  delay(1000);
 }
